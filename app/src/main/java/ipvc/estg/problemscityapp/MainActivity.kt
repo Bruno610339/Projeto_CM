9098b0da6bef1925430f19169f50d45e5443c194
Bruno610339 <bmiguelgomes@ipvc.pt>
2021-03-15 15:26:30 +0000

    first changes for doing three point menu

--- app/src/main/java/ipvc/estg/problemscityapp/MainActivity.kt
+++ app/src/main/java/ipvc/estg/problemscityapp/MainActivity.kt
@@ -1,11 +1,34 @@
 package ipvc.estg.problemscityapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    //Three Points Menu ----------------------------------------------------------------------------
+
+    //Function to call the menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == R.id.help) {
+
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+    //----------------------------------------------------------------------------------------------
 }
