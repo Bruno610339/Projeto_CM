8b3b89355607967ffda137cc211ad94400d393fc
Bruno610339 <bmiguelgomes@ipvc.pt>
2021-03-18 15:38:33 +0000

    RecyclerView for Notes done

--- app/src/main/java/ipvc/estg/problemscityapp/NotesActivity.kt
+++ app/src/main/java/ipvc/estg/problemscityapp/NotesActivity.kt
@@ -2,8 +2,12 @@ package ipvc.estg.problemscityapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import ipvc.estg.problemscityapp.adapter.LineAdapter
+import ipvc.estg.problemscityapp.dataclasses.Notes
+import kotlinx.android.synthetic.main.notes.*
 
-class NotesActivity : AppCompatActivity() {
+class NotesActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +18,22 @@ class NotesActivity : AppCompatActivity() {
         actionBar!!.title = "Notes"
         actionBar.setDisplayHomeAsUpEnabled(true)
 
+        //RecyclerView
+        val list = generateExample(10)
 
+        recycler_view.adapter = LineAdapter(list)
+        recycler_view.layoutManager = LinearLayoutManager(this)
+    }
 
+    //Função para gerar linhas para a recyclerView
+    private fun generateExample(size: Int): ArrayList<Notes> {
+        val list = ArrayList<Notes>()
+
+        for (i in 0 until size) {
+            val item = Notes("$i", "adfbadbf")
+            list += item
+        }
+
+        return list
     }
 }
---
+++ app/src/main/java/ipvc/estg/problemscityapp/adapter/LineAdapter.kt
@@ -0,0 +1,36 @@
+package ipvc.estg.problemscityapp.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import ipvc.estg.problemscityapp.R
+import ipvc.estg.problemscityapp.dataclasses.Notes
+
+class LineAdapter(private val list: ArrayList<Notes>): RecyclerView.Adapter<LineViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LineViewHolder {
+        val itemView = LayoutInflater
+                .from(parent.context)
+                .inflate(R.layout.recycler_notes_line, parent, false)
+
+        return LineViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: LineViewHolder, position: Int) {
+        val currentNote = list[position]
+
+        holder.title.text = currentNote.title
+        holder.description.text = currentNote.description
+    }
+}
+
+class LineViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    val title: TextView = itemView.findViewById(R.id.title_content)
+    val description: TextView = itemView.findViewById(R.id.description_content)
+}
