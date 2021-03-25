b3bfe2d838c8d973c8b9086c6456606d1ab3edb2
Bruno610339 <bmiguelgomes@ipvc.pt>
2021-03-25 14:40:51 +0000

    some changes

--- app/src/main/java/ipvc/estg/problemscityapp/LoginActivity.kt
+++ app/src/main/java/ipvc/estg/problemscityapp/LoginActivity.kt
@@ -11,11 +11,17 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
+        //Disable title bar
+        val actionBar = supportActionBar
+        actionBar!!.hide()
+
+        //Button to go for MainActivity after Login
         btn_login.setOnClickListener {
             val intent = Intent (this, MainActivity::class.java)
             startActivity(intent)
         }
 
+        //Button to go for NotesActivity for check Personal Notes
         btn_notas.setOnClickListener {
             val intent = Intent (this, NotesActivity::class.java)
             startActivity(intent)
