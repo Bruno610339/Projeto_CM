fc05f418a142084ae3516850daeebde41d49c54e
Bruno610339 <bmiguelgomes@ipvc.pt>
2021-03-25 20:57:52 +0000

    some changes

---
+++ app/src/main/java/ipvc/estg/problemscityapp/db/NoteDB.kt
@@ -0,0 +1,67 @@
+package ipvc.estg.problemscityapp.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import ipvc.estg.problemscityapp.dao.NoteDao
+import ipvc.estg.problemscityapp.entities.Note
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import java.text.DateFormat
+import java.util.*
+
+
+@Database(entities = arrayOf(Note::class), version = 7, exportSchema = false)
+public abstract class NoteDB: RoomDatabase() {
+
+    abstract fun noteDao(): NoteDao
+
+    private class NoteDatabaseCallback(private val scope: CoroutineScope) : RoomDatabase.Callback() {
+
+        override fun onOpen(db: SupportSQLiteDatabase) {
+            super.onOpen(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    val noteDao = database.noteDao()
+
+                    //Delete all content here
+                    //noteDao.deleteAll()
+
+                    //Add sample words
+                    var note = Note(1, "Note 1", "Sou uma description", DateFormat.getDateInstance().format(Date()))
+                    noteDao.insert(note)
+                    note = Note(2, "Note 2", "Sou uma description 2", DateFormat.getDateInstance().format(Date()))
+                    noteDao.insert(note)
+                }
+            }
+        }
+    }
+
+    companion object {
+        @Volatile
+        private var INSTANCE: NoteDB? = null
+
+        fun getDatabase(context: Context, scope: CoroutineScope): NoteDB {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        NoteDB::class.java,
+                        "note_database"
+                )
+                //Destruction Strategy
+                //.fallbackToDestructiveMigration()
+                .addCallback(NoteDatabaseCallback(scope))
+                .build()
+
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
--- app/src/main/java/ipvc/estg/problemscityapp/entities/Note.kt
+++ app/src/main/java/ipvc/estg/problemscityapp/entities/Note.kt
@@ -3,13 +3,12 @@ package ipvc.estg.problemscityapp.entities
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.util.*
 
 @Entity(tableName = "note_table")
 
 class Note(
-    @PrimaryKey(autoGenerate = true) val id: Int? = null,
-    @ColumnInfo(name = "title") val title: String,
-    @ColumnInfo(name = "description") val description: String,
-    @ColumnInfo(name = "date") val date: Date
+        @PrimaryKey(autoGenerate = true) val id: Int? = null,
+        @ColumnInfo(name = "title") val title: String,
+        @ColumnInfo(name = "description") val description: String,
+        @ColumnInfo(name = "date") val date: String
 )
