a84b9bea2a835425615968fcd9f9381d31fc655e
Bruno610339 <bmiguelgomes@ipvc.pt>
2021-05-01 17:08:56 +0100

    test

---
+++ app/src/main/java/ipvc/estg/problemscityapp/api/Report.kt
@@ -0,0 +1,13 @@
+package ipvc.estg.problemscityapp.api
+
+import java.util.*
+
+data class Report (
+    val id: Int,
+    val title: String,
+    val description: String,
+    val lat: Float,
+    val lng: Float,
+    val date_creation: Date,
+    val user_id: Int
+)
